7192b475cef1684f1f63c630650952ec106d89bf
Smorci <szekelyschmarton@gmail.com>
2024-02-16 20:34:09 +0000

    Remove unsolicited functions and respect instructions

--- Week-01/Day-01_Convert-Ages-To-Days/convert_ages_to_days_smorci/src/main.rs
+++ Week-01/Day-01_Convert-Ages-To-Days/convert_ages_to_days_smorci/src/main.rs
@@ -1,98 +1,44 @@
 use std::io;
-use chrono::{NaiveDate, TimeDelta, Utc};
-use std::str::Split;
-use std::str::FromStr;
 
 const NUMBER_OF_DAYS_IN_A_YEAR: u32 = 365;
 
-struct Date {
-    year: i32,
-    month: u32,
-    day: u32,
-}
+fn convert_years_to_days (years: u32) -> u32 {
 
-fn convert_years_to_days () {
-
-    println!("Converting years to days...");
-    println!();
-    println!("Please enter the number of years to convert:");
-    let years_as_string  = get_input().unwrap();
-
-    let years_as_u32 = convert_string_to_u32(years_as_string.clone());
-
-    let years_in_days = NUMBER_OF_DAYS_IN_A_YEAR * years_as_u32;
-
-    println!("{years} years, is equal to {years_in_days} days", years = years_as_string.trim());
-}
-
-fn convert_string_to_u32 (input: String) -> u32 {
-
-    let value = u32::from_str(&input.trim()).unwrap();
-
-    value
+    NUMBER_OF_DAYS_IN_A_YEAR * years
 
 }
 
-fn get_input () -> io::Result<String> {
+fn convert_string_to_u32 (string: &mut String) -> u32 {
 
-    let mut buffer = String::new();
+    let result: u32 = string.trim().parse().expect("Error parsing the input to unsigned integer");
+    result
+
+}
+
+fn get_input (buffer: &mut String) -> io::Result<()> {
 
     let stdin = io::stdin();
 
-    let _ = stdin.read_line(&mut buffer)?;
-    Ok(buffer)
-}
+    stdin.read_line(buffer)?;
 
-fn calculate_days_on_earth () {
-
-    println!("Please enter your birthday (yyyy-mm-dd):");
-
-    let birthday: String = get_input().unwrap();
-
-    let mut input_date: Date = Date{
-        year: 0,
-        month: 0,
-        day: 0,
-    };
-
-    let date_elements: Split<&str> = birthday.split("-");
-
-    for (i, element) in date_elements.enumerate() {
-        match i {
-            0 => input_date.year = i32::from_str(&element.trim()).unwrap(),
-            1 => input_date.month = u32::from_str(&element.trim()).unwrap(),
-            2 => input_date.day = u32::from_str(&element.trim()).unwrap(),
-            _ => panic!("Wrong input buy user! Follow instructions and respect the date format."),
-        }
-    }
-    
-    let birth_date = NaiveDate::from_ymd_opt(input_date.year, input_date.month, input_date.day).unwrap(); // NaiveDate because we dont
-                                                                                                          // know the timezone
-
-    let date_now: NaiveDate = Utc::now().date_naive();
-
-    let duration_since_birthday: TimeDelta = NaiveDate::signed_duration_since(date_now, birth_date);
-
-    let number_of_days_on_earth: i64 = duration_since_birthday.num_days();
-
-    println!("You have been alive for {number_of_days_on_earth} days!")
+    Ok(())
 }
 
 fn main () {
 
+    let mut buffer: String = String::new();
+
     println!("Welcome to age converter! This program converts ages to days.");
     println!();
-    println!("Select an option");
-    println!("1: Convert years to days");
-    println!("2: Calculate how many days I spent on earth");
+    println!("Enter the number of years to convert:");
+    
+    let _ = get_input(&mut buffer);
 
-    let option: &str = &get_input().unwrap();
+    let years_as_u32: u32 = convert_string_to_u32(&mut buffer);
 
-    match option.trim() {
-        "1" => convert_years_to_days(),
-        "2" => calculate_days_on_earth(),
-        _   => panic!("Invalid option. Please try again and enter the number of the option.")
-    }
+    let years_in_days = convert_years_to_days(years_as_u32);
+    let buffer_trimmed = buffer.trim();
+    println!("{buffer_trimmed} years is equal to {years_in_days} days!")
 
 }
 
