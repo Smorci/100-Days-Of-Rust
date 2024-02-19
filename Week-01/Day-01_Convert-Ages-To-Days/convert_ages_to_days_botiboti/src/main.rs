2dd7ade596497872c870cfdca1e492a2a8dc56a8
Botond Ferenczi <botondferenczi98@gmail.com>
2024-02-19 13:50:06 +0000

    tests

--- Week-01/Day-01_Convert-Ages-To-Days/convert_ages_to_days_botiboti/src/main.rs
+++ Week-01/Day-01_Convert-Ages-To-Days/convert_ages_to_days_botiboti/src/main.rs
@@ -1,8 +1,17 @@
 use num_bigint::{BigUint, ToBigUint};
 use std::io;
 
+// convert input string to BigUint
+fn read_parse(inp: String) -> BigUint {
+    let age: BigUint = inp.trim().parse().expect("Not an integer.");
+    age
+}
+
+// turn age in years to days
 fn calc_days(age: BigUint) -> BigUint {
-    let days_in_a_year: BigUint = 365_i32.to_biguint().expect("Something went wrong with the conversion i32 -> BigUint.");
+    let days_in_a_year: BigUint = 365_i32
+        .to_biguint()
+        .expect("Something went wrong with the conversion i32 -> BigUint.");
     age * days_in_a_year
 }
 
@@ -15,7 +24,40 @@ fn main() {
         .read_line(&mut age)
         .expect("Failed to read line.");
 
-    let age: BigUint = age.trim().parse().expect("Not an integer.");
+    let age: BigUint = read_parse(age);
     println!("You entered: {}.", age);
     println!("That's roughly {} days.", calc_days(age));
+}
+
+#[test]
+fn an_ordinary_human_age() {
+    let result = read_parse(String::from("50"));
+    let expectation: BigUint = 50_i32
+        .to_biguint()
+        .expect("Something went wrong with the conversion 50 : i32 -> BigUint.");
+    assert_eq!(result, expectation);
+}
+
+#[test]
+fn age_of_earth() {
+    let result = read_parse(String::from("4500000000"));
+    let expectation: BigUint = 4500000000_u64
+        .to_biguint()
+        .expect("Something went wrong with the conversion 4.5 billion: u64 -> BigUint.");
+    assert_eq!(result, expectation);
+}
+
+#[test]
+#[should_panic]
+fn not_a_positive_integer() {
+    read_parse(String::from("boo"));
+}
+
+#[test]
+fn calc_days_for_earth() {
+    let result = calc_days(read_parse(String::from("4500000000")));
+    let expectation: BigUint = 1642500000000_u64
+        .to_biguint()
+        .expect("Something went wrong with the conversion 13.5 billion: u64 -> BigUint.");
+    assert_eq!(result, expectation);
 }
