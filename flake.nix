81c02d401ae38c4dc8a7a87c46054c5ebe1820b1
Botond Ferenczi <botondferenczi98@gmail.com>
2024-02-20 11:38:01 +0000

    removed formatted flake from pr

--- flake.nix
+++ flake.nix
@@ -10,13 +10,12 @@
       let
         pkgs = import nixpkgs { inherit system; };
         naersk-lib = pkgs.callPackage naersk { };
-      in {
+      in
+      {
         defaultPackage = naersk-lib.buildPackage ./.;
-        devShell = with pkgs;
-          mkShell {
-            buildInputs =
-              [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
-            RUST_SRC_PATH = rustPlatform.rustLibSrc;
-          };
+        devShell = with pkgs; mkShell {
+          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
+          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+        };
       });
 }
