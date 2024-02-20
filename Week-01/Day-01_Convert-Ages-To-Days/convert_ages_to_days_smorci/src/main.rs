1206fed0e82af563e89ccb3ec151dd81b15dd382
Smorci <szekelyschmarton@gmail.com>
2024-02-20 21:49:17 +0000

    Add .envrc, use Result and resolve review

--- Week-01/Day-01_Convert-Ages-To-Days/convert_ages_to_days_smorci/src/main.rs
+++ Week-01/Day-01_Convert-Ages-To-Days/convert_ages_to_days_smorci/src/main.rs
@@ -1,4 +1,4 @@
-use num_bigint::BigUint;
+use num_bigint::{BigUint, ParseBigIntError};
 use std::io;
 use std::str::FromStr;
 
@@ -8,34 +8,40 @@ fn convert_years_to_days(years: BigUint) -> BigUint {
     NUMBER_OF_DAYS_IN_A_YEAR * years
 }
 
-fn convert_string_to_biguint(string: &mut String) -> BigUint {
-    let result = BigUint::from_str(string.trim()).expect("Error parsing the input to unsigned integer. Maybe the number you entered is negative, or it has alphabetical characters");
-    result
+fn convert_string_to_biguint(string: &str) -> Result<BigUint, ParseBigIntError> {
+
+    let result = BigUint::from_str(string.trim())?;
+
+    Ok(result)
 }
 
-fn get_input(buffer: &mut String) -> io::Result<()> {
+fn get_input() -> io::Result<String> {
+
+    let mut buffer = String::new();
+
     let stdin = io::stdin();
 
-    stdin.read_line(buffer)?;
+    stdin.read_line(&mut buffer)?;
 
-    Ok(())
+    Ok(buffer)
 }
 
 fn main() {
-    let mut buffer: String = String::new();
 
     println!("Welcome to age converter! This program converts ages to days.");
     println!();
     println!("Enter the number of years to convert:");
 
-    let _ = get_input(&mut buffer);
+    let buffer: String = get_input().expect("Invalid input. Provide only a positive integer.");
 
-    let years_as_biguint: BigUint = convert_string_to_biguint(&mut buffer);
+    let years: BigUint = convert_string_to_biguint(&buffer).expect("Error converting string to biguint");
 
-    let years_in_days = convert_years_to_days(years_as_biguint);
-    let buffer_trimmed = buffer.trim();
-    println!("{buffer_trimmed} years is equal to {years_in_days} days!")
+    let days: BigUint = convert_years_to_days(years);
+
+    let buffer_trimmed: &str = buffer.trim();
+    println!("{buffer_trimmed} years is equal to {days} days!")
 }
+
 #[cfg(test)]
 mod tests {
 
@@ -44,27 +50,27 @@ mod tests {
     #[test]
     fn test_big_integer() {
         let test_number = BigUint::from_str("20000000000000000000")
-            .expect("Invalid string of digits in BigUint test");
+            .unwrap();
         assert_eq!(
             convert_years_to_days(test_number),
             BigUint::from_str("7300000000000000000000")
-                .expect("Invalid string of digits in BigUint test")
+                .unwrap()
         );
     }
 
     #[test]
     fn test_string_to_biguint() {
-        let mut input: String = String::from("123");
+        let input = String::from("123");
         assert_eq!(
-            convert_string_to_biguint(&mut input),
-            BigUint::from_str("123").expect("Invalid string of digits in conversion test")
+            convert_string_to_biguint(&input).unwrap(),
+            BigUint::from_str("123").unwrap()
         );
     }
 
     #[test]
     #[should_panic]
     fn test_conversion_parse_int_error() {
-        let mut input: String = String::from("abc");
-        convert_string_to_biguint(&mut input);
+        let input = String::from("abc");
+        convert_string_to_biguint(&input).unwrap();
     }
 }
