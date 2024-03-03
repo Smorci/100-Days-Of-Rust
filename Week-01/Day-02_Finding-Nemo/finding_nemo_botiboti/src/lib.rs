46c67328aad10482d42e1855d31daab478d4c4bb
Botond Ferenczi <botondferenczi98@gmail.com>
2024-03-03 01:14:36 +0000

    added doc

--- Week-01/Day-02_Finding-Nemo/finding_nemo_botiboti/src/lib.rs
+++ Week-01/Day-02_Finding-Nemo/finding_nemo_botiboti/src/lib.rs
@@ -1,13 +1,18 @@
+//! Week-01/Day-02 solution.
+
+/// Returns the index of the word "Nemo" in the input string or None if "Nemo" is not found.
 pub fn get_index(inp: &str) -> Option<usize> {
     inp.split_whitespace().position(|x| x == "Nemo")
 }
 
+/// Returns a message with the position of "Nemo" if it's found.
 pub fn index_to_string(index: Option<usize>) -> String {
     index.map_or("I can't find Nemo :(".to_string(), |i| {
         "I found Nemo at ".to_string() + &(i + 1).to_string() + "!"
     })
 }
 
+/// Finds "Nemo" in a given string and returns a message about whether and where it was found.
 pub fn find_nemo(inp: &str) -> String {
     index_to_string(get_index(inp))
 }
