53d294590b6d7a6bc047915d2349795908a790e7
Smorci <szekelyschmarton@gmail.com>
2024-03-03 19:39:18 +0000

    Doc strings and single functionality coverage crate

--- Week-01/Day-01_Convert-Ages-To-Days/convert_ages_to_days_smorci/src/lib.rs
+++ Week-01/Day-01_Convert-Ages-To-Days/convert_ages_to_days_smorci/src/lib.rs
@@ -1,28 +1,15 @@
-use num_bigint::{BigUint, ParseBigIntError};
-use std::io;
-use std::str::FromStr;
+//! Convert years to days.
+//!
+//! Provides a way to return the number of days in a year
+use num_bigint::BigUint;
 
-// Number of days in a year
+/// Number of days in a year
 pub const NUMBER_OF_DAYS_IN_A_YEAR: u32 = 365;
 
-// Takes a year as an argument and returns the equivalent in days
-// Example:
-// convert_years_to_days(1) -> 365
-pub fn convert_years_to_days(years: BigUint) -> BigUint {
+/// Takes a year as an argument and returns the equivalent in days
+/// convert_years_to_days(1) -> 365
+pub fn convert_years_to_days(years: &BigUint) -> BigUint {
     NUMBER_OF_DAYS_IN_A_YEAR * years
-}
-
-// Takes a &str type and returns either the string parsed as a BigUint or an Error
-pub fn convert_string_to_biguint(string: &str) -> Result<BigUint, ParseBigIntError> {
-    BigUint::from_str(string.trim())
-}
-
-// Prompts the user to provide input through stdin and returns said input as String or an Error
-pub fn get_input() -> io::Result<String> {
-    let mut buffer = String::new();
-    let stdin = io::stdin();
-    stdin.read_line(&mut buffer)?;
-    Ok(buffer)
 }
 
 // Unit tests
@@ -30,32 +17,32 @@ pub fn get_input() -> io::Result<String> {
 mod tests {
 
     use super::*;
+    use std::str::FromStr;
 
     // Tests a number bigger than u64::MAX
     #[test]
     fn test_big_integer() {
         let test_number = BigUint::from_str("20000000000000000000").unwrap();
         assert_eq!(
-            convert_years_to_days(test_number),
+            convert_years_to_days(&test_number),
             BigUint::from_str("7300000000000000000000").unwrap()
         );
     }
 
-    // Tests a conversion from str to BigUint
+    // Tests arbitrary numbers
     #[test]
-    fn test_string_to_biguint() {
-        let input = String::from("123");
+    fn test_integer() {
         assert_eq!(
-            convert_string_to_biguint(&input).unwrap(),
-            BigUint::from_str("123").unwrap()
+            convert_years_to_days(&BigUint::from(10_u32)),
+            BigUint::from_str("3650").unwrap()
         );
-    }
-
-    // Tests a faulty input
-    #[test]
-    #[should_panic(expected = "ParseBigIntError")]
-    fn test_conversion_parse_int_error() {
-        let input = String::from("abc");
-        convert_string_to_biguint(&input).unwrap();
+        assert_eq!(
+            convert_years_to_days(&BigUint::from(1_u32)),
+            BigUint::from_str("365").unwrap()
+        );
+        assert_eq!(
+            convert_years_to_days(&BigUint::from(40_u32)),
+            BigUint::from_str("14600").unwrap()
+        );
     }
 }
--- Week-01/Day-01_Convert-Ages-To-Days/convert_ages_to_days_smorci/src/main.rs
+++ Week-01/Day-01_Convert-Ages-To-Days/convert_ages_to_days_smorci/src/main.rs
@@ -1,16 +1,22 @@
-use convert_ages_to_days_smorci::{convert_years_to_days, convert_string_to_biguint, get_input};
+use convert_ages_to_days_smorci::convert_years_to_days;
 use num_bigint::BigUint;
+use std::io;
+use std::str::FromStr;
 
 fn main() {
     println!("Welcome to age converter! This program converts ages to days.");
     println!();
     println!("Enter the number of years to convert:");
 
-    let buffer: String = get_input().expect("Unable to read input.");
-    let years: BigUint = convert_string_to_biguint(&buffer)
+    let mut buffer: String = String::new();
+
+    io::stdin()
+        .read_line(&mut buffer)
+        .expect("Unable to read input.");
+
+    let years: BigUint = BigUint::from_str(buffer.trim())
         .expect("Could not parse year. Please enter a positive integer.");
-    let days: BigUint = convert_years_to_days(years.clone());
+    let days: BigUint = convert_years_to_days(&years);
 
     println!("{years} years is equal to {days} days!")
 }
-
