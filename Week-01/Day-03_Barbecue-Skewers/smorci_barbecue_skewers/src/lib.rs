0bfc3ee65cd9b201646e05ee72344c353c039bfa
Smorci <szekelyschmarton@gmail.com>
2024-05-08 17:53:43 +0100

    Use files

--- Week-01/Day-03_Barbecue-Skewers/smorci_barbecue_skewers/src/lib.rs
+++ Week-01/Day-03_Barbecue-Skewers/smorci_barbecue_skewers/src/lib.rs
@@ -1,6 +1,7 @@
 pub fn count_skewers(buffer: &str) -> [usize; 2] {
     let mut skewers = skewer_utils::parse_skewers_to_vec(&buffer);
-    [skewer_utils::count_vegetarians(&mut skewers), skewer_utils::count_meats(&mut skewers)]
+    let mut skewers_clone = skewers.clone();
+    [skewer_utils::count_vegetarians(&mut skewers), skewer_utils::count_meats(&mut skewers_clone)]
 }
 
 mod skewer_utils {
--- Week-01/Day-03_Barbecue-Skewers/smorci_barbecue_skewers/src/main.rs
+++ Week-01/Day-03_Barbecue-Skewers/smorci_barbecue_skewers/src/main.rs
@@ -1,19 +1,16 @@
-use std::io;
+use std::fs::File;
+use std::io::prelude::*;
 use smorci_barbecue_skewers::{count_skewers};
 
-fn main() {
-    println!("Please enter a barbecue skewer delimited by ','");
+fn main() -> std::io::Result<()>{
 
-    let mut buffer: String = String::new();
-
-    // Input from the terminal doesn't really work, because it take an Enter (newline) as the end
-    // of input. Let's start working with files, more beneficial IMO
-    io::stdin()
-        .read_line(&mut buffer)
-        .expect("Error reading input from stdin.");
+    let mut file = File::open("data/skewer.txt")?;
+    let mut buffer = String::new();
+    file.read_to_string(&mut buffer)?;
 
     let skewer_count = count_skewers(&buffer);
 
     println!("The number of vegetarian and non-vegetarian skewers are [{},{}], respectively", skewer_count[0], skewer_count[1]);
 
+    Ok(())
 }
